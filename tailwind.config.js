7faa70a969d425102a21a646b1912b579ffbdf8e
Stephanie Dennehy <stephanie_dennehy@icloud.com>
2024-07-21 19:06:08 -0400

    Add README, update text sizes and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,15 @@ module.exports = {
       fontFamily: {
         sans: ['Barlow Semi Condensed', 'sans-serif'],
       },
+      colors: {
+        moderateViolet: 'hsl(263, 55%, 52%)',
+        veryDarkGrayishBlue: 'hsl(217, 19%, 35%)',   
+        veryDarkBlackishBlue: 'hsl(219, 29%, 14%)',
+        white: 'hsl(0, 0%, 100%)',
+        lightGray: 'hsl(0, 0%, 81%)',
+        lightGrayishBlue: 'hsl(210, 46%, 95%)',
+
+      }
     },
   },
   plugins: [require('@tailwindcss/line-clamp')],
